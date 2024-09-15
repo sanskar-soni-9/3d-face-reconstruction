cd9898b707619623755f4b68dbce3be84123ad4d
Sanskar Soni <sanskarsoni300@gmail.com>
2024-09-15 23:24:37 +0530

    feat(dataset): add homepage url
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
 // 300W-LP dataset: http://www.cbsr.ia.ac.cn/users/xiangyuzhu/projects/3DDFA/Database/300W-LP/main.htm
+// homepage: http://www.cbsr.ia.ac.cn/users/xiangyuzhu/projects/3DDFA/main.htm
 
 pub const DEFAULT_DATA_DIR: &str = "data/";
