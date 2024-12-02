e4c8f19e540c0ae69b74105c27fe3529806a150a
Sanskar Soni <sanskarsoni300@gmail.com>
2024-12-02 21:25:41 +0530

    refactor: organize config file
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -1,16 +1,26 @@
 // 300W-LP dataset: http://www.cbsr.ia.ac.cn/users/xiangyuzhu/projects/3DDFA/Database/300W-LP/main.htm
 // homepage: http://www.cbsr.ia.ac.cn/users/xiangyuzhu/projects/3DDFA/main.htm
 
-pub const DEFAULT_LEARNING_RATE: f64 = 0.256;
-pub const MINI_BATCH_SIZE: usize = 4;
-pub const BATCH_EPSILON: f64 = 1e-5;
-pub const NORM_MOMENTUM: f64 = 0.99;
+/// Model configurations
+// General
+pub const CONV_WEIGHT_SCALE: f64 = 2.;
+pub const DENSE_WEIGHT_SCALE: f64 = 2.;
+pub const BATCH_EPSILON: f64 = 1.001e-5;
+
+// EfficientNet
 pub const SE_RATIO: f64 = 0.25; // Squeeze & Excite ratio
+
+/// Trainig configurations
+// General
+pub const DEFAULT_LEARNING_RATE: f64 = 0.0001;
+pub const MINI_BATCH_SIZE: usize = 3;
 pub const DROPOUT_RATE: f64 = 0.2;
+pub const NORM_MOMENTUM: f64 = 0.99;
+
+// Sgd with momentum
 pub const SGD_MOMENTUM: f64 = 0.9;
 
-pub const INPUT_SHAPE: (usize, usize, usize) = (3, 224, 224); // (channles {3: RGB}, height, width)
-pub const DATASET_INPUT_SIZE: usize = 450; // 450x450
+/// Training labels
 pub const TRAINIG_LABELS: [&str; 1] = [
     "pts_2d", // 136
              // "pt2d",       // 136
@@ -24,11 +34,15 @@ pub const TRAINIG_LABELS: [&str; 1] = [
              // "roi",        // 4
 ];
 pub const CNN_OUTPUT_SIZE: usize = 136;
-pub const CONV_WEIGHT_SCALE: f64 = 2.;
-pub const DENSE_WEIGHT_SCALE: f64 = 2.;
+
+/// Miscellaneous configurations
 pub const PRETTY_SAVE: bool = false;
 
-// Dirs & Paths
+/// Model inputs configurations
+pub const INPUT_SHAPE: (usize, usize, usize) = (3, 224, 224); // (channles {3: RGB}, height, width)
+pub const DATASET_INPUT_SIZE: usize = 450; // 450x450
+
+// Dirs & Files
 pub const OUTPUT_DIR: &str = "results";
 pub const DATA_DIR: &str = "data";
 pub const MODELS_DIR: &str = "models";
