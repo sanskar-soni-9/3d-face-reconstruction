b2b0feff7945c318c09a1aa98df05ed4c0c2e532
Sanskar Soni <sanskarsoni300@gmail.com>
2024-12-02 21:28:43 +0530

    feat(utils): add elementwise_max function
    Signed-off-by: Sanskar Soni <sanskarsoni300@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use ndarray::{Array, Dimension};
+use rayon::iter::{ParallelBridge, ParallelIterator};
+
 pub fn vec_f32_to_array_f64<const N: usize>(vec: &[f32]) -> [f64; N] {
     let mut vec_iter = vec.iter();
     [0.0; N].map(|_| {
@@ -29,4 +32,16 @@ pub fn str_to_vec_f64(s: &str) -> Result<Vec<f64>, std::num::ParseFloatError> {
         .split(',')
         .map(|x| x.trim().parse::<f64>())
         .collect()
+}
+
+pub fn elementwise_max<D>(mtrx_a: &Array<f64, D>, mtrx_b: &Array<f64, D>) -> Array<f64, D>
+where
+    D: Dimension,
+{
+    let mut max = mtrx_a.to_owned();
+    max.iter_mut()
+        .zip(mtrx_b.iter())
+        .par_bridge()
+        .for_each(|(val_max, val_b)| *val_max = val_max.max(*val_b));
+    max
 }
